367573a63408e4bac61cdc3be5dfe47b86143d87
Samuel Oliveira <161156540+SmileyTEX@users.noreply.github.com>
2026-04-29 00:34:03 +0000

    feat: simulação dos dados services.ts

--- rpgwiki/src/app/services/personagem.service.ts
+++ rpgwiki/src/app/services/personagem.service.ts
@@ -6,33 +6,66 @@ import { Personagem } from '../models/personagem';
 })
 export class PersonagemService {
 
-  private personagens: Personagem[] = [
-    { id: 1, nome: 'Arthas', nivel: 10, ativo: true },
-    { id: 2, nome: 'Merlin', nivel: 20, ativo: false }
-  ];
+  private personagens: Personagem[] = [];
+  private nextId = 1;
 
-  private nextId = 3;
+  constructor() {
+    this.carregarDoLocalStorage();
+  }
 
-  listar(): Personagem[] {
+  
+  private carregarDoLocalStorage() {
+    const dados = localStorage.getItem('personagens');
+
+    if (dados) {
+      this.personagens = JSON.parse(dados);
+
+     
+      const maiorId = this.personagens.reduce((max, p) => p.id > max ? p.id : max, 0);
+      this.nextId = maiorId + 1;
+
+    } else {
+      
+      this.personagens = [
+        {
+          id: 1,
+          nome: 'Arthas',
+          nivel: 10,
+          ativo: true,
+          classe: 'Guerreiro',
+          descricao: 'Um cavaleiro caído em busca de redenção.',
+          imagem: 'https://i.pinimg.com/736x/e0/21/be/e021be95108888de62ab5ac48ccd7a03.jpg'
+        }
+      ];
+      this.nextId = 2;
+      this.salvarNoLocalStorage();
+    }
+  }
+
+  
+  private salvarNoLocalStorage() {
+    localStorage.setItem('personagens', JSON.stringify(this.personagens));
+  }
+
+  listar() {
     return this.personagens;
   }
 
-  buscarPorId(id: number): Personagem | undefined {
-    return this.personagens.find(p => p.id === id);
+  adicionar(p: Omit<Personagem, 'id'>) {
+    this.personagens.push({ ...p, id: this.nextId++ });
+    this.salvarNoLocalStorage(); 
   }
 
-  adicionar(personagem: Omit<Personagem, 'id'>) {
-    this.personagens.push({ ...personagem, id: this.nextId++ });
-  }
-
-  atualizar(personagem: Personagem) {
-    const index = this.personagens.findIndex(p => p.id === personagem.id);
-    if (index !== -1) {
-      this.personagens[index] = personagem;
+  atualizar(p: Personagem) {
+    const i = this.personagens.findIndex(x => x.id === p.id);
+    if (i !== -1) {
+      this.personagens[i] = p;
+      this.salvarNoLocalStorage(); 
     }
   }
 
   remover(id: number) {
     this.personagens = this.personagens.filter(p => p.id !== id);
+    this.salvarNoLocalStorage(); 
   }
 }
